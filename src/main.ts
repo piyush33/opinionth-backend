4567e6cc41631d7bc2b5f3672b338699211a43f0
Khidki Tech <khidkitech@Khidkis-MacBook-Air.local>
2025-11-25 23:30:32 +0530

    fix: cors

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,18 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
   app.enableCors({
-    origin: '*', // Replace with your frontend domains
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: [
+      'https://www.opinionth.com',
+      'https://opinionth.com',
+      'http://localhost:3000',   // dev origins as needed
+      'http://localhost:5173',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
   await app.listen(3000);
 }
 bootstrap();
